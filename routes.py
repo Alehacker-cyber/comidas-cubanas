10218deb3a2e0b865260c6a24caeebf0a43ca74d
Alehacker-cyber <beatonprohenzaa@gmail.com>
2025-08-17 16:47:40 -0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,28 @@
-from app import login_manager
+from flask import request, redirect, url_for, render_template, flash
+from flask_login import login_user, logout_user, login_required
 from models import User
+from app import app
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Credenciales incorrectas')
+    return render_template('login.html')
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
