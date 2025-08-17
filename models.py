aa81d3a5c1cf8b56a58af537fd193b7f8777b21c
Alehacker-cyber <beatonprohenzaa@gmail.com>
2025-08-17 17:33:01 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(10))  # 'admin' o 'cliente'
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dish = db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     notes = db.Column(db.Text)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
