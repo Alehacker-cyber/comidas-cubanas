a96f06f8f1a8b3f2d8068d9710fc77c2ba74ef6e
Alehacker-cyber <beatonprohenzaa@gmail.com>
2025-08-17 11:55:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,12 @@ from routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 @app.route('/set_language', methods=['POST'])
 def set_language():
     lang = request.form.get('language')
     session['lang'] = lang
     return redirect(request.referrer or url_for('index'))
+
 
 
