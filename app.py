e5d373b57635f1ac5cfe72e89bd9a79315f5333d
Alehacker-cyber <beatonprohenzaa@gmail.com>
2025-08-17 16:35:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ db.init_app(app)  # ✅ inicializa la instancia importada
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -38,4 +33,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
